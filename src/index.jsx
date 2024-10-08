1121e868785e299346115519b84ee9afbf9ac247
Glitch (glitch-hello-react) <none>
2024-10-08 10:01:47 +0000

    💑🌊 Checkpoint ./src/pages/app.jsx:9475993/2272 ./src/index.jsx:9475993/8

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./app.jsx";
+import App from "./pages/app.jsx";
 import './styles/index.css'; // Import any global CSS styles
 import './styles/index3d.css'; // Import 
 
--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,21 +1,26 @@
 // App.js
-import React from 'react';
-import Header from './components/Header';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HeroSection from './components/HeroSection';
-import Featurette from './components/Featurette';
-import ContactForm from './components/ContactForm';
-import Footer from './components/Footer';
+import React from "react";
+import Header from "../components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HeroSection from "../components/HeroSection";
+import Featurette from "../components/Featurette";
+import ContactForm from "../components/ContactForm";
+import Footer from "../components/Footer";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/index.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/index.css";
 
 function App() {
   return (
-    
     <div className="App">
       <Header />
       <main>
+        <Routes>
+          <Route path="/" element={<HeroSection />} />
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/features" element={<Featurette />} />
+          <Route path="/contact" element={<ContactForm />} />
+        </Routes>
         <HeroSection />
         <Featurette />
         <ContactForm />
