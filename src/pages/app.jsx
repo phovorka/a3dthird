b94d05a7894e1479a583506c69925670c66f1463
Glitch (glitch-hello-react) <none>
2024-10-08 13:33:07 +0000

    🐔🍅 Checkpoint ./src/pages/app.jsx:9475993/562

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -8,20 +8,19 @@ import ContactForm from "../components/ContactForm";
 import Footer from "../components/Footer";
 import ModelViewer from "../components/ModelViewer";
 import Demo from "./demo";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import '@google/model-viewer'; // Import the model-viewer library
-
 import "../styles/index.css";
-
+import "../styles/index3d.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function App() {
   return (
     <Router>
-      {" "}
-      {/* Ensure your Router wraps everything */}
       <div className="App">
         <Header />
           <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} /> 
             <Route path="/demo" element={<Demo />} />
           </Routes>
         <Footer />
