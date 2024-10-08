3bca5120551c04f527653fd522c0d21c38c63855
Glitch (glitch-hello-react) <none>
2024-10-08 08:42:15 +0000

    🦄🌽 Checkpoint ./src/index.jsx:9475993/3 ./package.json:9475993/2

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./app.jsx";
 import './styles/index.css'; // Import any global CSS styles
 import './styles/index3d.css'; // Import 
