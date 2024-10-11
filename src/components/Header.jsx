f7ec6f4e6757db1585b8475c51f4695ba56bccdc
Glitch (glitch-hello-react) <none>
2024-10-11 08:40:21 +0000

    👩‍❤️‍💋‍👩🚌 Checkpoint ./src/components/Header.jsx:9475993/259

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <div className="container py-3">
-        <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
+      <div className="container ">
+        <div className="d-flex flex-column flex-md-row align-items-center  mb-2 border-bottom">
           <Link to="/" className="d-flex align-items-center text-dark text-decoration-none">
             <img
               src="https://cdn.glitch.global/2bc6ab97-e692-4373-99f6-6e1f98a13434/Assembly.svg?v=1715948982135"
@@ -15,15 +15,15 @@ const Header = () => {
             />
           </Link>
           <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-            <Link className="me-3 py-2 text-dark text-decoration-none" to="/features">
+            <Link className="me-3 py-2 text-dark text-decoration-none" to="#">
               Výhody
             </Link>
             <Link className="me-3 py-2 text-dark text-decoration-none" to="/pages/demo">
               Demo
             </Link>
-            <Link className="py-2 text-dark text-decoration-none" to="#contact">
-              Kontakt
-            </Link>
+<a className="py-2 text-dark text-decoration-none" href="/#contact">
+  Kontakt
+</a>
           </nav>
         </div>
       </div>
