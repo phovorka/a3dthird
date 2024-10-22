bb61ca7b3ff6912a9b06b119948b7f445176681e
Glitch (glitch-hello-react) <none>
2024-10-22 12:31:18 +0000

    💫🎖 Checkpoint ./src/components/HeroSection.jsx:9475993/646 ./src/locales/cs/translation.json:9475993/61 ./src/locales/en/translation.json:9475993/66 ./src/styles/index3d.css:9475993/1165

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,20 +9,12 @@ const HeroSection = () => {
       <div className="container">
         <div className="row justify-content-center p-0 p-sm-5">
           <div className="col-12 col-lg-8 col-xl-6 text-center p-4">
-            <h1 className="text-white display-5 fw-bolder">
+            <h1 className="lead text-primary-500 display-5 fw-bolder">
               {t('heroSection.title')} {/* Translated title */}
             </h1>
-            <p className="lead text-white-50 px-2 mb-4">
+            <p className="lead text-white text-white-50 px-2 mb-4">
               {t('heroSection.subtitle')} {/* Translated subtitle */}
             </p>
-            <a href="demo.html">
-              <button className="btn btn-primary btn-lg px-4 m-0 mb-3 mx-sm-3">
-                {t('heroSection.demoButton')} {/* Translated demo button */}
-              </button>
-            </a>
-            <button className="btn btn-outline-light btn-lg px-4 m-0 mb-3 mx-sm-3">
-              {t('heroSection.advantagesButton')} {/* Translated advantages button */}
-            </button>
           </div>
         </div>
       </div>
