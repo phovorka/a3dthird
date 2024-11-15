9d7f8f0e62fe25c43e939e073cd0636cd1b1dd06
phovorka <hovorkapetr@seznam.cz>
2024-11-15 10:10:27 +0100

    transl

--- src/components/Featurette.jsx
+++ src/components/Featurette.jsx
@@ -5,7 +5,7 @@ import ModelViewer from './ModelViewer';
 const Featurette = ({ heading, subheading, description, modelSrc, modelId }) => {
   return (
     <div className="container">
-      <div className="row featurette py-5 px-3">
+      <div className="row featurette pb-3">
         <div className="col-md-7">
           <h2 className="featurette-heading fw-normal lh-1">
             {heading} <span className="text-muted">{subheading}</span>
