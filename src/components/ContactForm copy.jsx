7adf87f360e12795038e4618246741e30415acd5
phovorka <hovorkapetr@seznam.cz>
2024-11-19 13:17:25 +0100

    contact change

---
+++ src/components/ContactForm copy.jsx
@@ -0,0 +1,73 @@
+// components/ContactForm.js
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+const ContactForm = () => {
+  const { t } = useTranslation(); // Hook pro překlady
+
+  return (
+    <section className="py-4 bg-light pb-5 pt-5" id="contact">
+      <div className="container my-1">
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-5 col-md-8">
+            <h2 className="fw-bold">{t('contactForm.title')}</h2> {/* Přeložený nadpis */}
+         <form name="contact"  method="POST" data-netlify="true">
+              <div className="form-floating mb-3">
+                <input
+                  type="text"
+                  name="name"
+                  className="form-control"
+                  id="floatingInput-1"
+                  placeholder={t('contactForm.namePlaceholder')} // Přeložený placeholder
+                  required
+                />
+                <label htmlFor="floatingInput-1">{t('contactForm.name')}</label> {/* Přeložený label */}
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control"
+                  id="floatingInput-2"
+                  placeholder={t('contactForm.emailPlaceholder')} // Přeložený placeholder
+                  required
+                />
+                <label htmlFor="floatingInput-2">{t('contactForm.email')}</label> {/* Přeložený label */}
+              </div>
+              <div className="form-floating mb-3">
+                <input
+                  type="tel"
+                  name="phone"
+                  className="form-control"
+                  id="floatingInput-3"
+                  placeholder={t('contactForm.phonePlaceholder')} // Přeložený placeholder
+                  required
+                />
+                <label htmlFor="floatingInput-3">{t('contactForm.phone')}</label> {/* Přeložený label */}
+              </div>
+              <div className="form-floating">
+                <textarea
+                  className="form-control"
+                  id="floatingTextarea-4"
+                  name="text"
+                  placeholder={t('contactForm.messagePlaceholder')} // Přeložený placeholder
+                  required
+                  style={{ height: '150px' }}
+                ></textarea>
+                <label htmlFor="floatingTextarea-4">{t('contactForm.message')}</label> {/* Přeložený label */}
+              </div>
+              <div>
+                <button className="btn btn-primary w-100 py-2 mt-4 fs-5">
+                  {t('contactForm.submit')} {/* Přeložené tlačítko */}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+
+export default ContactForm;
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -11,57 +11,18 @@ const ContactForm = () => {
         <div className="row justify-content-center">
           <div className="col-12 col-lg-5 col-md-8">
             <h2 className="fw-bold">{t('contactForm.title')}</h2> {/* Přeložený nadpis */}
-         <form name="contact"  method="POST" data-netlify="true">
-              <div className="form-floating mb-3">
-                <input
-                  type="text"
-                  name="name"
-                  className="form-control"
-                  id="floatingInput-1"
-                  placeholder={t('contactForm.namePlaceholder')} // Přeložený placeholder
-                  required
-                />
-                <label htmlFor="floatingInput-1">{t('contactForm.name')}</label> {/* Přeložený label */}
-              </div>
-              <div className="form-floating mb-3">
-                <input
-                  type="email"
-                  name="email"
-                  className="form-control"
-                  id="floatingInput-2"
-                  placeholder={t('contactForm.emailPlaceholder')} // Přeložený placeholder
-                  required
-                />
-                <label htmlFor="floatingInput-2">{t('contactForm.email')}</label> {/* Přeložený label */}
-              </div>
-              <div className="form-floating mb-3">
-                <input
-                  type="tel"
-                  name="phone"
-                  className="form-control"
-                  id="floatingInput-3"
-                  placeholder={t('contactForm.phonePlaceholder')} // Přeložený placeholder
-                  required
-                />
-                <label htmlFor="floatingInput-3">{t('contactForm.phone')}</label> {/* Přeložený label */}
-              </div>
-              <div className="form-floating">
-                <textarea
-                  className="form-control"
-                  id="floatingTextarea-4"
-                  name="text"
-                  placeholder={t('contactForm.messagePlaceholder')} // Přeložený placeholder
-                  required
-                  style={{ height: '150px' }}
-                ></textarea>
-                <label htmlFor="floatingTextarea-4">{t('contactForm.message')}</label> {/* Přeložený label */}
-              </div>
-              <div>
-                <button className="btn btn-primary w-100 py-2 mt-4 fs-5">
-                  {t('contactForm.submit')} {/* Přeložené tlačítko */}
-                </button>
-              </div>
-            </form>
+
+            <div className="card text-center shadow-lg" style={{ width: '18rem', margin: '20px auto' }}>
+
+            <div className="card-body">
+                <h5 className="card-title">Petr Hovorka</h5>
+                <p className="card-text">{t('contactForm.phonePlaceholder')}</p>
+                <p className="card-text text-muted">{t('contactForm.emailPlaceholder')} </p>
+
+            </div>
+        </div>
+
+
           </div>
         </div>
       </div>
