61c701a1cca191de3cf4d9b4ba24adc2a54ca93c
phovorka <hovorkapetr@seznam.cz>
2025-07-22 09:50:23 +0200

    zmena s novym branch

---
+++ src/pages/configurator.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Config3d from '../components/Config3d';
+
+const Configurator = () => {
+    return (
+       <div className="Configurator">
+              <Config3d />
+
+        </div>
+    );
+  };
+  
+  export default Configurator;
