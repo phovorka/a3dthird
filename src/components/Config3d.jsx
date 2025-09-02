0c7db3c8f92788da1a11f94c250abfd7fdd6a600
hovorkapetr <hovorkapetr@googlemail.com>
2025-09-02 09:40:59 +0000

    config chng

--- src/components/Config3d.jsx
+++ src/components/Config3d.jsx
@@ -1,26 +1,18 @@
 // src/components/Config3D.jsx
 // content for Configurator page 
+
 import React, { useState, useEffect, useRef,useMemo } from "react";
 import "@google/model-viewer"; 
 import { useTranslation } from 'react-i18next'; 
 import './config3d.module.css';
 
 const Config3d = () => {
-
   const { t } = useTranslation();
-
   return (
     <div className="container text-center my-5">
-      <h2 className="fw-bold text-primary">{t("comparison.title")}</h2>
-      <h2 className="fw-bold text-primary">{t("comparison.subtitle")}</h2>
-      <p className="text-muted">
-        {t("comparison.description")}
-      </p>
-
+      <h2 className="fw-bold text-primary">{t("configurator.title")}</h2>
     </div>
   );
 };
-
-
 
     export default Config3d;
