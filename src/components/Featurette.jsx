ee2e3f91685aaec11f0d3916a23ba4d9abba9f0c
hovorkapetr <hovorkapetr@googlemail.com>
2025-09-02 10:02:20 +0000

    css chng Featurrete

--- src/components/Featurette.jsx
+++ src/components/Featurette.jsx
@@ -5,7 +5,7 @@ import ModelViewer from './ModelViewer';
 const Featurette = ({ heading, subheading, description, modelSrc, modelId }) => {
   return (
     <div className="container">
-      <div className="row featurette">
+      <div className="row featurette pt-4">
         <div className="col-md-7 pt-4">
           <h2 className="featurette-heading fw-normal lh-2">
             {heading} <span className="text-muted ">{subheading}</span>
